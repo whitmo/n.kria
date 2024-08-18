4df72f7824996a1c240810ed427c62a44e20a86d
Whit Morriss <code@morriss.rocks>
2024-08-18 14:05:19 -0500

    transport cleanup
    - fix refs
    - move note_clock into transport
    - dispatch pattern for modal movement handlers

--- lib/transport.lua
+++ lib/transport.lua
@@ -1,6 +1,9 @@
 local tu = require 'tabutil'
+local tab = tu
+local mu = require 'musicutil'
 
 local Transport = {}
+
 
 function Transport:init(ctx, data, defaults) -- @@ explode or create special .init_via_ctx function
    self.data = data
@@ -14,6 +17,71 @@ function Transport:from_ctx(ctx)
    self:init(ctx, ctx.data, ctx.defaults)
    return self
 end
+
+function Transport:current_val(track, page)
+   return self.defaults.value_buffer[track][page]
+end
+
+function Transport:note_clock(track)
+   local data = self.data
+   local ctx = self.ctx
+   local player = data:get_player(track)
+   local slide_or_modulate = self:current_val(track,'slide') -- to match stock kria times
+   local velocity = self:current_val(track,'velocity')
+   local divider = data:get_page_val(track,'trig','divisor')
+   local subdivision = self:current_val(track,'retrig')
+   local gate_len = self:current_val(track,'gate')
+   local gate_multiplier = data:get_track_val(track,'gate_shift')
+   local duration = util.clamp(gate_len-1, 0, 4)/16
+   if gate_len == 1 or gate_len == 6 then
+      duration = duration + 0.02 -- this turns the longest notes into ties, and the shortest into blips, at mult of 1
+   else
+      duration = duration - 0.02
+   end
+   duration = duration * gate_multiplier
+   -- print('repeating note '..subdivision..' times')
+   for i=1,subdivision do
+      if data:get_subtrig(track,data:get_pos(track,'retrig'),i)==1 then
+	 if data:get_track_val(track,'trigger_clock') == 1 then
+	    for _,v in pairs(self.defaults.trigger_clock_pages) do
+	       self:advance_page(track,v)
+	    end
+	 end
+	 local description = player:describe()
+	 self.meta:update_last_notes()
+	 local note = description.style == 'kit' and ctx.last_notes_raw[track] or ctx.last_notes[track]
+	 player:play_note(note, (velocity-1)/6, duration/subdivision)
+
+	 if ctx.matrix ~= nil then
+	    ctx.matrix:set("pitch_t"..track, (note - 36)/(127-36))
+	    ctx.matrix:set('trig_t'..track,1)
+	    ctx.matrix:set('trig_t'..track,0)
+	 end
+
+	 local note_str
+	 if description.style == 'kit' then
+	    note_str = ''
+	    for x=0,note,3 do
+	       note_str = ' ' .. note_str
+	    end
+
+	    note_str = note_str..note
+	 else
+	    note_str = mu.note_num_to_name(note, true)
+	 end
+	 if description.supports_slew then
+	    local slide_amt = util.linlin(1,7,1,120,slide_or_modulate) -- to match stock kria times
+	    player:set_slew(slide_amt/1000)
+	 else
+	    local num = util.linlin(1,7,0,1,slide_or_modulate)
+	    player:modulate(num)
+	 end
+	 ctx.screen_graphics:add_history(track, note_str, clock.get_beats())
+      end
+      clock.sleep(clock.get_beat_sec()*divider/(4*subdivision))
+   end
+end
+
 
 function Transport:play_pause()
    self.data:delta_global_val('playing', 1)
@@ -36,7 +104,7 @@ function Transport:reset_all()
 end
 
 function Transport:reset_track(t)
-   for k,v in ipairs(self.defaults.combined_page_list) do
+   for _,v in ipairs(self.defaults.combined_page_list) do
       if v == 'scale' or v == 'patterns' then break end
       self:reset_page(t,v)
    end
@@ -70,101 +138,119 @@ function Transport:advance_all()
 end
 
 function Transport:advance_pattern_page()
-   self.data:delta_global_val('pattern_quant_pos',1)
+   -- @@ move to data functions
+   local data = self.data
+   data:delta_global_val('pattern_quant_pos',1)
 
-   if self.data:get_global_val('pattern_quant_pos') <= self.data:get_global_val('pattern_quant') then return end
+   if data:get_global_val('pattern_quant_pos') <= self.data:get_global_val('pattern_quant') then return end
 
-   self.data:set_global_val('pattern_quant_pos',1)
-   if self.data:get_global_val('cued_pattern') ~= 0 then
-      self.data:set_global_val('active_pattern',self.data:get_global_val('cued_pattern'))
-      self.data:set_global_val('cued_pattern',0)
-      self.ctx:post('pattern '..ap()..' active')
+   data:set_global_val('pattern_quant_pos',1)
+   if data:get_global_val('cued_pattern') ~= 0 then
+      data:set_global_val('active_pattern',data:get_global_val('cued_pattern'))
+      data:set_global_val('cued_pattern',0)
+      self.ctx:post('pattern '..data:ap()..' active')
    end
 
    if self.data:get_global_val('ms_active') == 0 then return end
 
    self.data:delta_global_val('ms_duration_pos',1)
-   if self.data:get_global_val('ms_duration_pos') > self.data:get_global_val('ms_duration_'..self.data:get_global_val('ms_pos')) then
-      self.data:set_global_val('ms_duration_pos',1)
-      self.data:delta_global_val('ms_pos',1)
-      if self.data:get_global_val('ms_pos') > self.data:get_global_val('ms_last') or self.data:get_global_val('ms_pos') < self.data:get_global_val('ms_first') then
-	 self.data:set_global_val('ms_pos',1)
+   if self.data:get_global_val('ms_duration_pos') > data:get_global_val('ms_duration_'..data:get_global_val('ms_pos'))
+   then
+      data:set_global_val('ms_duration_pos',1)
+      data:delta_global_val('ms_pos',1)
+      if data:get_global_val('ms_pos') > data:get_global_val('ms_last')
+	 or data:get_global_val('ms_pos') < data:get_global_val('ms_first') then
+	 data:set_global_val('ms_pos',1)
       end
    end
-
-   self.data:set_global_val('active_pattern',self.data:get_global_val('ms_pattern_'..self.data:get_global_val('ms_pos')))
+   data:set_global_val('active_pattern', data:get_global_val('ms_pattern_'..data:get_global_val('ms_pos')))
 end
 
 function Transport:advance_track(t)
-   local pages_to_advance = (self.data:get_track_val(t,'trigger_clock') == 1) and {'trig','retrig'} or pages_with_steps
+   local data = self.data
+   local pages_to_advance = (data:get_track_val(t, 'trigger_clock') == 1)
+      and {'trig','retrig'}
+      or self.defaults.pages_with_steps
    local note_will_fire = false
    for k,v in pairs(pages_to_advance) do
       -- print('attempting to advance page',v)
-      self.data:delta_page_val(t,v,'counter',1)
-      if self.data:get_page_val(t,v,'counter') > self.data:get_page_val(t,v,'divisor') then
-	 self.data:set_page_val(t,v,'counter',1)
-	 if type(self.data.tracks[t][v].temp_loop_first) == 'number' then
+      data:delta_page_val(t,v,'counter',1)
+      if data:get_page_val(t,v,'counter') > data:get_page_val(t,v,'divisor') then
+	 data:set_page_val(t,v,'counter',1)
+	 if type(data.tracks[t][v].temp_loop_first) == 'number' then
 	    if self:advance_page(t,v, false, true) then note_will_fire = true end
 	    self:advance_page(t, v, true, false)
-	    -- print(t, v, "fake", self.data:get_pos(t, v), "real", self.data:get_page_val(t,v,'pos'))
+	    -- print(t, v, "fake", data:get_pos(t, v), "real", data:get_page_val(t,v,'pos'))
 	 else
 	    if self:advance_page(t,v, true, true) then note_will_fire = true end
 	 end
       end
    end
-   if note_will_fire then clock.run(note_clock,t) end
+   if note_will_fire then clock.run(function (track) self:note_clock(track) end, t) end
 end
 
+Transport.modal_page_handlers = {
+    forward = function (tp, old, real, first, _, t, p)
+      local new = old + 1
+      if tp.data:out_of_bounds(t,p,new, real) then
+	 return first, true
+      end
+         return new, false
+    end,
+    reverse = function (tp, old, real, _, last, t, p)
+      local new = old - 1
+      if tp.data:out_of_bounds(t,p,new, real) then
+	 return last, true
+      end
+      return new, false
+    end,
+    triangle = function (tp, old, real, first, last, t, p)
+       local delta = tp.data:get_page_val(t,p,'pipo_dir') == 1 and 1 or -1
+       local new = old + delta
+       if tp.data:out_of_bounds(t,p,new, real) then
+	  if new > last then
+	     new = util.clamp(last-1,first,last)
+	     tp.data:set_page_val(t,p,'pipo_dir',0)
+	  elseif new < first then
+	     new = util.clamp(first+1,first,last)
+	     tp.data:set_page_val(t,p,'pipo_dir',1)
+	  end
+	  return new, true
+       end
+       return new, false
+    end,
+    drunk = function (_, old, _, first, last, _, _)
+       local delta
+       local new = nil
+       if new == first then delta = 1
+       elseif new == last then delta = -1
+       else delta = math.random() > 0.5 and 1 or -1
+       end
+       new = old + delta
+       if new > last then
+	  new = last
+       elseif new < first then
+	  new = first
+       end
+       -- ^ have to do it this way vs out_of_bounds() because we want to get to the closest boundary, not necessarily first or last step in loop.
+       return new, false
+    end,
+    random = function (_, _, _, first, last, _, _, _)
+       local new  = util.round(math.random(first, last))
+       return new, false
+    end
+}
+
 function Transport:advance_page(t,p,real,playing) -- track,page
+   local ctx = self.ctx
    local old_pos = real and self.data:get_page_val(t,p,'pos') or self.data:get_pos(t, p)
    local first = real and self.data:get_page_val(t,p,'loop_first') or self.data:get_loop_first(t, p)
    local last = real and self.data:get_page_val(t,p,'loop_last') or self.data:get_loop_last(t, p)
    local mode = self.defaults.play_modes[self.data:get_track_val(t,'play_mode')]
-   local new_pos;
-   local resetting = false
+   local new_pos
+   local resetting
 
-   if mode == 'forward' then
-      new_pos = old_pos + 1
-      if self.data:out_of_bounds(t,p,new_pos, real) then
-	 new_pos = first
-	 resetting = true
-      end
-   elseif mode == 'reverse' then
-      new_pos = old_pos - 1
-      if self.data:out_of_bounds(t,p,new_pos, real) then
-	 new_pos = last
-	 resetting = true
-      end
-   elseif mode == 'triangle' then
-      local delta = self.data:get_page_val(t,p,'pipo_dir') == 1 and 1 or -1
-      new_pos = old_pos + delta
-      if self.data:out_of_bounds(t,p,new_pos, real) then
-	 if new_pos > last then
-	    new_pos = util.clamp(last-1,first,last)
-	    self.data:set_page_val(t,p,'pipo_dir',0)
-	 elseif new_pos < first then
-	    new_pos = util.clamp(first+1,first,last)
-	    self.data:set_page_val(t,p,'pipo_dir',1)
-	 end
-	 resetting = true
-      end
-   elseif mode == 'drunk' then
-      local delta
-      if new_pos == first then delta = 1
-      elseif new_pos == last then delta = -1
-      else delta = math.random() > 0.5 and 1 or -1
-      end
-      new_pos = old_pos + delta
-      if new_pos > last then
-	 new_pos = last
-      elseif new_pos < first then
-	 new_pos = first
-      end
-      -- ^ have to do it this way vs out_of_bounds() because we want to get to the closest boundary, not necessarily first or last step in loop.
-
-   elseif mode == 'random' then
-      new_pos = util.round(math.random(first,last))
-   end
+   new_pos, resetting = self.modal_page_handlers[mode](self, old_pos, real, first, last, t, p)
 
    if resetting and playing and self.data:get_page_val(t,p,'cued_divisor') ~= 0 then
       self.data:set_page_val(t,p,'divisor',self.data:get_page_val(t,p,'cued_divisor'))
@@ -177,13 +263,14 @@ function Transport:advance_page(t,p,real,playing) -- track,page
       self.data.tracks[t][p].temp_pos = new_pos
    end
 
+   local prob_map = self.ctx.defaults.prob_map
    if playing and math.random(0,99) < prob_map[self.data:get_step_val(t,p,self.data:get_pos(t,p), 'prob')] then
-      if matrix and tab.contains(matrix_sources,p) then
-	 matrix:set(p..'_t'..t, (self.data:get_step_val(t,p,self.data:get_pos(t,p))-1)/6)
+      if ctx.matrix and tab.contains(ctx.matrix_sources,p) then
+	 ctx.matrix:set(p..'_t'..t, (self.data:get_step_val(t,p,self.data:get_pos(t,p))-1)/6)
       end
       if self.data:get_track_val(t,'mute') == 0 then
-	 value_buffer[t][p] = self.data:get_step_val(t,p,self.data:get_pos(t,p))
-	 if p == 'trig' and current_val(t,'trig') == 1 then
+	 ctx.value_buffer[t][p] = self.data:get_step_val(t,p,self.data:get_pos(t,p))
+	 if p == 'trig' and self:current_val(t,'trig') == 1 then
 	    return true
 	 end
       end
--- norkria.lua
+++ norkria.lua
@@ -34,34 +34,25 @@ WHAT GOES IN THIS FILE:
 
 ]]--
 
-
-local defaults = include('lib/defaults')
-local Prms = include('lib/prms')
-local ctx = include("lib/context")
-
--- hardware
-g = grid.connect()
-m = midi.connect()
-
-ctx:preinit(defaults)
-
-local screen_graphics = include('lib/screen_graphics')
-
-
-local Onboard = include('lib/onboard')
-local gkeys = include('lib/gkeys')
-local meta = include('lib/meta')
-
-local transport = include('lib/transport')
-local hs = include('lib/dualdelay')
-local nb = include("lib/nb/lib/nb")
 local mu = require 'musicutil'
 local tu = require 'tabutil'
 local tab = tu
 
+local defaults = include('lib/defaults')
+local ctx = include("lib/context")
+
+ctx:preinit(defaults)
+
+local screen_graphics = include('lib/screen_graphics')
+local meta = include('lib/meta')
+local transport = include('lib/transport')
+local hs = include('lib/dualdelay')
+local nb = include("lib/nb/lib/nb")
 
 function init()
-   local ctx = ctx:init()
+   ctx:init()
+
+   function ctx.grid.key(x,y,z) ctx.gkeys:key(x,y,z) end
 
    nb.voice_count = 4
    nb:init()
@@ -75,78 +66,18 @@ function redraw() ctx.screen_graphics:render() end
 
 function key(n,d) ctx:key(n,d) end
 function enc(n,d) ctx:enc(n,d) end
-function g.key(x,y,z) ctx.gkeys:key(x,y,z) end
 
-function clock.transport.start() ctx.data:set_global_val('playing',1); ctx.post('play') end
-function clock.transport.stop() ctx.data:global_set_val('playing',0); ctx.post('stop') end
+function clock.transport.start()
+   ctx.data:set_global_val('playing', 1)
+   ctx:post('play')
+end
+
+function clock.transport.stop()
+   ctx.data:global_set_val('playing', 0)
+   ctx:post('stop')
+end
 
 function cleanup()
    Data = nil
    norns.state.context.nkria = nil
-end
-
-function menu_clock(n)
-   blink.menu[n] = true
-   clock.sleep(1/4)
-   blink.menu[n] = false
-end
-
-function note_clock(track)
-   local player = data:get_player(track)
-   local slide_or_modulate = current_val(track,'slide') -- to match stock kria times
-   local velocity = current_val(track,'velocity')
-   local divider = data:get_page_val(track,'trig','divisor')
-   local subdivision = current_val(track,'retrig')
-   local gate_len = current_val(track,'gate')
-   local gate_multiplier = data:get_track_val(track,'gate_shift')
-   local duration = util.clamp(gate_len-1, 0, 4)/16
-   if gate_len == 1 or gate_len == 6 then
-      duration = duration + 0.02 -- this turns the longest notes into ties, and the shortest into blips, at mult of 1
-   else
-      duration = duration - 0.02
-   end
-   duration = duration * gate_multiplier
-   -- print('repeating note '..subdivision..' times')
-   for i=1,subdivision do
-      if data:get_subtrig(track,data:get_pos(track,'retrig'),i)==1 then
-	 if data:get_track_val(track,'trigger_clock') == 1 then
-	    for _,v in pairs(trigger_clock_pages) do transport:advance_page(track,v) end
-	 end
-	 local description = player:describe()
-	 meta:update_last_notes()
-	 local note = description.style == 'kit' and last_notes_raw[track] or last_notes[track]
-	 player:play_note(note, (velocity-1)/6, duration/subdivision)
-
-	 if matrix ~= nil then
-	    matrix:set("pitch_t"..track, (note - 36)/(127-36))
-	    matrix:set('trig_t'..track,1)
-	    matrix:set('trig_t'..track,0)
-	 end
-
-	 local note_str
-	 if description.style == 'kit' then
-	    note_str = ''
-	    for x=0,note,3 do
-	       note_str = ' ' .. note_str
-	    end
-
-	    note_str = note_str..note
-	 else
-	    note_str = mu.note_num_to_name(note, true)
-	 end
-	 if description.supports_slew then
-	    local slide_amt = util.linlin(1,7,1,120,slide_or_modulate) -- to match stock kria times
-	    player:set_slew(slide_amt/1000)
-	 else
-	    local num = util.linlin(1,7,0,1,slide_or_modulate)
-	    player:modulate(num)
-	 end
-	 screen_graphics:add_history(track, note_str, clock.get_beats())
-      end
-      clock.sleep(clock.get_beat_sec()*divider/(4*subdivision))
-   end
-end
-
-function current_val(track, page)
-   return ctx.defaults.value_buffer[track][page]
 end
